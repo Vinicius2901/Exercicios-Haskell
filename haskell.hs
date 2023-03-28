6a798e45a123c651a3fbdfbe208e6ca06ca62094
Vinícius Giroti <121032547+Vinicius2901@users.noreply.github.com>
2023-03-28 13:34:11 -0300

    Add files via upload

---
+++ haskell.hs
@@ -0,0 +1,21 @@
+ehTriangulo a b c | b >= c && a > (b - c) && a < (b + c) = "True"
+                  | c >= b && a > (c - b) && a < (b + c) = "True"
+                  | a >= c && b > (a - c) && b < (a + c) = "True"
+                  | c >= a && b > (c - a) && b < (a + c) = "True"
+                  | a >= b && c > (a - b) && c < (a + b) = "True"
+                  | b >= a && c > (b - a) && c < (a + b) = "True"
+                  | otherwise = "False"
+
+tipoTriangulo a b c | a == b && b == c = "equilatero"
+                    | a /= b && a /= c && b /= c = "escaleno"
+                    | otherwise = "isosceles"
+
+triangulo a b c | ehTriangulo a b c == "False" = "nao eh triangulo"
+                | otherwise = tipoTriangulo a b c
+
+somapares 0 = 0
+somapares n | mod n 2 == 0 = n + somapares (n - 2)
+            | otherwise = somapares (n - 1)
+
+potencia 0 m = m
+potencia n m = 2^(n - 1) * m + 2^n * m
