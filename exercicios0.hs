1e135f99540f42058826b97498c67b7958db991d
Vinícius Giroti <121032547+Vinicius2901@users.noreply.github.com>
2023-03-30 16:58:34 -0300

    Add files via upload

---
+++ exercicios0.hs
@@ -0,0 +1,12 @@
+funcao x = x^2 + 2*x + 3
+
+modulo x y z = sqrt (x^2 + y^2 + z^2)
+
+cem x | x > 100 = True
+      | otherwise = False
+
+soma x y z w = x+y+z+w
+
+vetores x y z x' y' z' = (x*x' + y*y' + z*z')
+
+porcentagem x y = (x / y) * 100
