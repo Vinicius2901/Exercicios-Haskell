014a1036788c2d0a2ceb7f3494e930c88dca8930
Vinícius Giroti <121032547+Vinicius2901@users.noreply.github.com>
2023-03-30 22:24:45 -0300

    Update haskell.hs

--- haskell.hs
+++ haskell.hs
@@ -1,27 +1,42 @@
-ehTriangulo a b c | b >= c && a > (b - c) && a < (b + c) = "True"
-                  | c >= b && a > (c - b) && a < (b + c) = "True"
-                  | a >= c && b > (a - c) && b < (a + c) = "True"
-                  | c >= a && b > (c - a) && b < (a + c) = "True"
-                  | a >= b && c > (a - b) && c < (a + b) = "True"
-                  | b >= a && c > (b - a) && c < (a + b) = "True"
-                  | otherwise = "False"
+--Exercício 1
+
+ehTriangulo a b c | b >= c && a > (b - c) && a < (b + c) = True
+                  | c >= b && a > (c - b) && a < (b + c) = True
+                  | a >= c && b > (a - c) && b < (a + c) = True
+                  | c >= a && b > (c - a) && b < (a + c) = True
+                  | a >= b && c > (a - b) && c < (a + b) = True
+                  | b >= a && c > (b - a) && c < (a + b) = True
+                  | otherwise = False
+
+--Exercício 2
 
 tipoTriangulo a b c | a == b && b == c = "equilatero"
                     | a /= b && a /= c && b /= c = "escaleno"
                     | otherwise = "isosceles"
 
-triangulo a b c | ehTriangulo a b c == "False" = "nao eh triangulo"
+--Exercício 3
+
+triangulo a b c | ehTriangulo a b c == False = "nao eh triangulo"
                 | otherwise = tipoTriangulo a b c
+
+--Exercício 4
 
 somapares 0 = 0
 somapares n | mod n 2 == 0 = n + somapares (n - 2)
             | otherwise = somapares (n - 1)
 
+--Exercício 5
+
 potencia 0 m = m
-potencia n m = 2^n * m + potencia (n - 1) m
+potencia n m = 2^n * m + potencia (n - 1) m 
+
+--Exercício 6
 
 primo n = primo' n (n - 1)
 
-primo' n 1 = "True"
-primo' n x | mod n x == 0 = "False"
-           | otherwise = primo' n (x - 1)
+primo' x 1 = True
+primo' x y | mod x y == 0 = False
+           | otherwise = primo' x (y - 1)
+
+--Exercício 7
+
