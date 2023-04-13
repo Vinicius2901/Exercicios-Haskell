6a5c8dd87788dd11dd3fb9672680f113279cde3f
Vinícius Giroti <121032547+Vinicius2901@users.noreply.github.com>
2023-04-13 15:37:54 -0300

    Add files via upload

---
+++ Lista1_vinicius_giroti.hs
@@ -0,0 +1,46 @@
+--Exercício 1
+
+ehTriangulo a b c | b >= c && a > (b - c) && a < (b + c) = True
+                  | c >= b && a > (c - b) && a < (b + c) = True
+                  | a >= c && b > (a - c) && b < (a + c) = True
+                  | c >= a && b > (c - a) && b < (a + c) = True
+                  | a >= b && c > (a - b) && c < (a + b) = True
+                  | b >= a && c > (b - a) && c < (a + b) = True
+                  | otherwise = False
+
+--Exercício 2
+
+tipoTriangulo a b c | a == b && b == c = "equilatero"
+                    | a /= b && a /= c && b /= c = "escaleno"
+                    | otherwise = "isosceles"
+
+--Exercício 3
+
+triangulo a b c | ehTriangulo a b c == False = "nao eh triangulo"
+                | otherwise = tipoTriangulo a b c
+
+--Exercício 4
+
+somapares 0 = 0
+somapares n | mod n 2 == 0 = n + somapares (n - 2)
+            | otherwise = somapares (n - 1)
+
+--Exercício 5
+
+potencia 0 m = m
+potencia n m = 2^n * m + potencia (n - 1) m 
+
+--Exercício 6
+
+primo n = primo' n (n - 1)
+
+primo' x 1 = True
+primo' x y | mod x y == 0 = False
+           | otherwise = primo' x (y - 1)
+
+--Exercício 7
+
+
+seriePI n = seriePI' n 1 1 --ultimo 1 é o sinal
+seriePI' n denominador sinal | denominador < n = 4/denominador * sinal + seriePI' n (denominador + 2) (sinal * (-1))
+                             | otherwise = 0
