49d8fb35eba0a2e84814ed95618066b1b788003b
Vinícius Giroti <121032547+Vinicius2901@users.noreply.github.com>
2023-04-18 16:59:58 -0300

    Create vetores.hs

--- vetores.hs
+++ vetores.hs
@@ -11,4 +11,26 @@ duplicalist (x:xs) = x : x : duplicalist xs
 
 impares [] = []
 impares (x:xs) | mod x 2 == 0 = impares xs
-                | otherwise = x : impares xs 
+               | otherwise = x : impares xs 
+
+--Exercício 4
+
+nprimeiros 0 _ = []
+nprimeiros n [] = []
+nprimeiros n (x:xs) = x : nprimeiros (n-1) xs
+
+--Explicação:
+--nprimeiros 2 [1,2,3] = 1 : nprimeiros 1 [2,3]
+--nprimeiros 1 [2,3] = 2 : nprimeiros 0 [3]
+--nprimeiros 0 [3] = []
+--1:2:[] = [1,2]
+
+--Exercício 5
+
+nultimos n [] = []
+nultimos n (x:xs) = reverse (nprimeiros n (reverse (x:xs)))
+
+--Exercício 6
+
+inverso [] = []
+inverso (x:xs) = 
