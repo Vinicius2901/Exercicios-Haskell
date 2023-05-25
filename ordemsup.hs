1f7ef2226e13a9d269a3e236baebf15ab9ee2e21
Vinícius Giroti <121032547+Vinicius2901@users.noreply.github.com>
2023-05-25 17:00:19 -0300

    Create ordemsup.hs

---
+++ ordemsup.hs
@@ -0,0 +1,32 @@
+import Data.Char
+
+transforma _ [] = []
+transforma f (x:xs) = f x : transforma f xs
+
+all2min x = toLower x
+
+all2mai x = toUpper x
+
+num2string x | x == 0 = "zero"
+             | x == 1 = "um"
+             | x == 2 = "dois"
+             | x == 3 = "tres"
+             | x == 4 = "quatro"
+             | x == 5 = "cinco"
+             | x == 6 = "seis"
+             | x == 7 = "sete"
+             | x == 8 = "oito"
+             | x == 9 = "nove"
+             | otherwise = "Num sei"
+
+aplicarsobre _ [] = []
+aplicarsobre f (x:xs) = f x : aplicarsobre f xs
+
+quadrado x = x^2
+
+posicao x = zip [1..length(x)] x
+
+divisao x = (snd x) `div` (fst x)
+
+reduzird f aux [] = aux
+reduzird f aux (x:xs) = f x (reduzird f aux xs)
