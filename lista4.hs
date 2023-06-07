aa88c81f550f5a8d1ca7f7ca352861ad0cb9b85d
Vinícius Giroti <121032547+Vinicius2901@users.noreply.github.com>
2023-06-07 16:45:17 -0300

    Update lista4.hs

--- lista4.hs
+++ lista4.hs
@@ -10,9 +10,32 @@ type Palavra = String
 pulalinha x | isSpace x = '\n'
             | otherwise = x
 
+numLinhas :: [Linha] -> [(Int, Linha)]
+numLinhas ls = numLinhas' ls 1
+
+numLinhas' [] _ = []
+numLinhas' (x:xs) n = (n, x) : numLinhas' xs (n+1)
+
+-- tirar pontuações
+palavras' [] = []
+palavras' (x:xs) | isAlpha x || isSpace x = x : palavras' xs
+                 | otherwise = palavras' xs
+
+-- tirar palavras <3
+palavras'' [] _ _ = []
+palavras'' (x:xs) c ultimaPalavra | isSpace x && c < 3 = palavras'' xs 0 []
+                                  | isSpace x = ultimaPalavra ++ [x] ++ palavras'' xs 0 []
+                                  | otherwise = palavras'' xs (c+1) (ultimaPalavra ++ [x])
+
+numeraPalavras [] = []
+numeraPalavras ((nlin,lin):proxlin) = (map ((nlin,)) (words lin)) ++ numeraPalavras proxlin
+
 main :: IO ()
 main = do
-    conteudo <- readFile "alice.txt"
-    let nlinhas = lines conteudo
-    let numLinhas = length nlinhas
-    print numLinhas
+    conteudo <- readFile "Alice.txt"
+    let semPontuacao = palavras' conteudo
+        maior3 = palavras'' semPontuacao 0 []
+        nlinhas = lines maior3
+        numeroLinhas = numLinhas nlinhas
+        numeraPal = numeraPalavras numeroLinhas
+    print numeraPal
